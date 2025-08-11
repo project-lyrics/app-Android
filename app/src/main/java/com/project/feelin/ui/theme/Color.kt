3a141ce2f4584f3f9a2bcd35e5a23420871d7bbe
gdaegeun539 <gdaegeun539@gmail.com>
2025-08-11 23:09:24 +0900

    design: add figma color palette

--- app/src/main/java/com/project/feelin/ui/theme/Color.kt
+++ app/src/main/java/com/project/feelin/ui/theme/Color.kt
@@ -2,10 +2,64 @@ package com.project.feelin.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightBrandPrimary = Color(0xFF6790F4)
+val LightBrandSecondary = Color(0xFFDBF3FF)
+val LightBrandTertiary = Color(0xFF20368D)
+val DarkBrandPrimary = Color(0xFF658CEA)
+val DarkBrandSecondary = Color(0xFF91B3E3)
+val DarkBrandTertiary = Color(0xFF142875)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightBackgroundPrimary = Color(0xFFFFFFFF)
+val LightBackgroundSecondary = Color(0xFFD2D4DA)
+val LightBackgroundTertiary = Color(0xFFF3F4F8)
+val DarkBackgroundPrimary = Color(0xFF0C0C0D)
+val DarkBackgroundSecondary = Color(0xFF36363F)
+val DarkBackgroundTertiary = Color(0xFF2A2A2E)
+
+val LightSystemModal = Color(0xFFFFFFFF)
+val DarkSystemModal = Color(0xFF494955)
+
+val CommonSystemDim = Color(0x10122366)
+
+val CommonPoint = Color(0xFFFA5454)
+
+val LightSystemInputField = Color(0xFFF3F4F8)
+val DarkSystemInputField = Color(0xFF222225)
+
+val LightSystemActivate = Color(0xFF6790F4)
+val DarkSystemActivate = Color(0xFF658CEA)
+val LightSystemDisable = Color(0xFFD2D4DA)
+val DarkSystemDisable = Color(0xFF36363F)
+
+val LightSystemPressedBrand = Color(0xFFEBF3FF)
+val DarkSystemPressedBrand = Color(0xFF91B3E3)
+val LightSystemPressedGreyScale = Color(0xFFF3F4F8)
+val DarkSystemPressedGreyScale = Color(0xFF6B6B79)
+
+val CommonAlertWarning = Color(0xFFF24242)
+val LightAlertWarning = Color(0xFF2B69FF)
+val DarkAlertWarning = Color(0xFF3C75FF)
+
+val LightSystemBorder = Color(0xFF101223)
+val DarkSystemBorder = Color(0xFF36363F)
+
+val LightGray00 = Color(0xFF000000)
+val LightGray01 = Color(0xFFF3F4F8)
+val LightGray02 = Color(0xFFD2D4DA)
+val LightGray03 = Color(0xFFB3B5BD)
+val LightGray04 = Color(0xFF9496A1)
+val LightGray05 = Color(0xFF777986)
+val LightGray06 = Color(0xFF5B5D6B)
+val LightGray07 = Color(0xFF404252)
+val LightGray08 = Color(0xFF282A3A)
+val LightGray09 = Color(0xFF101223)
+val DarkGray00 = Color(0xFF0C0C0D)
+val DarkGray01 = Color(0xFF2A2A2E)
+val DarkGray02 = Color(0xFF36363F)
+val DarkGray03 = Color(0xFF494955)
+val DarkGray04 = Color(0xFF6B6B79)
+val DarkGray05 = Color(0xFF747481)
+val DarkGray06 = Color(0xFF868691)
+val DarkGray07 = Color(0xFF9292A0)
+val DarkGray08 = Color(0xFFB7B7C5)
+val DarkGray09 = Color(0xFFDCDCE6)

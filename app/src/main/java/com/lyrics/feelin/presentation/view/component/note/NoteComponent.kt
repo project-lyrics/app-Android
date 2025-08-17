67b2ce08ac6957e42ea0d6dc944ac5dcd89ac09e
gdaegeun539 <gdaegeun539@gmail.com>
2025-08-17 22:24:17 +0900

    design: update note lyrics background
    - 배경이 꽉 차지 않는 문제를 수정합니다.
    - 배경 내부 가사 텍스트를 디자인에 맞게 중앙 정렬합니다.

--- app/src/main/java/com/lyrics/feelin/presentation/view/component/note/NoteComponent.kt
+++ app/src/main/java/com/lyrics/feelin/presentation/view/component/note/NoteComponent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,7 +69,7 @@ fun NoteComponent(noteData: NoteComponentData, modifier: Modifier = Modifier) {
             )
         }
         Text(
-            noteData.content,
+            text = noteData.content,
             overflow = TextOverflow.Ellipsis,
             maxLines = 3,
             style = MaterialTheme.typography.bodySmall.copy(color = LightGray09),
@@ -75,13 +77,17 @@ fun NoteComponent(noteData: NoteComponentData, modifier: Modifier = Modifier) {
         )
         if (noteData.lyrics != null)
             Box(
-                modifier = Modifier.width(350.dp).height(132.dp).padding(bottom = 16.dp),
+                modifier = Modifier.fillMaxWidth().height(132.dp).padding(bottom = 16.dp),
                 contentAlignment = Alignment.Center,
             ) {
                 LyricsBackground(background = noteData.lyrics.background)
                 Text(
                     text = noteData.lyrics.content,
-                    style = MaterialTheme.typography.bodyLarge.copy(color = LightGray09),
+                    style =
+                        MaterialTheme.typography.bodyLarge.copy(
+                            color = LightGray09,
+                            textAlign = TextAlign.Center,
+                        ),
                 )
             }
         HorizontalDivider(color = LightGray01, thickness = 1.dp)
@@ -164,6 +170,7 @@ private fun LyricsBackground(background: LyricsBackground, modifier: Modifier = 
     Image(
         painterResource(background.toImage()),
         contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxSize().clip(shape = RoundedCornerShape(size = 4.dp)),
         contentDescription = null,
     )
 }

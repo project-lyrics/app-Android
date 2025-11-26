2de3c8c1be4ce436fb9dc2db3b24f618f1c35c78
gdaegeun539 <gdaegeun539@gmail.com>
2025-11-26 15:50:01 +0900

    design: set the click color of the dialog button

--- app/src/main/java/com/lyrics/feelin/core/designsystem/component/FeelinModalDialog.kt
+++ app/src/main/java/com/lyrics/feelin/core/designsystem/component/FeelinModalDialog.kt
@@ -3,6 +3,8 @@ package com.lyrics.feelin.core.designsystem.component
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,15 +12,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,6 +47,9 @@ fun FeelinModalDialog(
 ) {
     val feelinColors = LocalFeelinColors.current
 
+    val confirmInteractionSource = remember { MutableInteractionSource() }
+    val isConfirmPressed by confirmInteractionSource.collectIsPressedAsState()
+
     // 모달 다이얼로그 제목만 디자인시스템 적용을 안 하네요?
     val titleTextStyle = TextStyle(
         fontFamily = pretendardFamily,
@@ -56,7 +63,8 @@ fun FeelinModalDialog(
     Dialog(onDismissRequest = {}) {
         Column(
             modifier = modifier.width(280.dp)
-                .background(color = feelinColors.modal, shape = RoundedCornerShape(size = 12.dp)),
+                .clip(shape = RoundedCornerShape(12.dp))
+                .background(color = feelinColors.modal),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
@@ -72,11 +80,19 @@ fun FeelinModalDialog(
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 isDismissButtonEnable.takeIf { it }?.let {
+                    val dismissInteractionSource = remember { MutableInteractionSource() }
+                    val isDismissPressed by dismissInteractionSource.collectIsPressedAsState()
+
                     Box(
                         modifier = Modifier
                             .weight(1f)
                             .height(52.dp)
-                            .clickable(onClick = onDismissButtonClick::invoke),
+                            .background(if (isDismissPressed) feelinColors.gray01 else feelinColors.modal)
+                            .clickable(
+                                interactionSource = dismissInteractionSource,
+                                indication = null,
+                                onClick = onDismissButtonClick::invoke
+                            ),
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
@@ -91,7 +107,12 @@ fun FeelinModalDialog(
                     modifier = Modifier
                         .weight(1f)
                         .height(52.dp)
-                        .clickable(onClick = onConfirmButtonClick::invoke),
+                        .background(if (isConfirmPressed) feelinColors.gray01 else feelinColors.modal)
+                        .clickable(
+                            interactionSource = confirmInteractionSource,
+                            indication = null,
+                            onClick = onConfirmButtonClick::invoke
+                        ),
                     contentAlignment = Alignment.Center
                 ) {
                     Text(

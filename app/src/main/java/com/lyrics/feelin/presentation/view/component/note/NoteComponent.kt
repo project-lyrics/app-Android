360054e0bbabb74cfd8e39d6b9a1608aa9296283
gdaegeun539 <gdaegeun539@gmail.com>
2025-12-28 19:49:05 +0900

    design: add music component's all design with data
    - Music 컴포넌트가 나타내야 하는 5가지의 상태를 sealed class로 추가
    - MusicComponent의 Slot API 기반 레이아웃을 제작
      - 각 컴포넌트 별로 맞는 레이아웃 컨텐츠를 추가

--- app/src/main/java/com/lyrics/feelin/presentation/view/component/note/NoteComponent.kt
+++ app/src/main/java/com/lyrics/feelin/presentation/view/component/note/NoteComponent.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,14 +26,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import com.lyrics.feelin.R
 import com.lyrics.feelin.presentation.designsystem.theme.FeelinTheme
 import com.lyrics.feelin.presentation.designsystem.theme.FeelinTypography
 import com.lyrics.feelin.presentation.designsystem.theme.LightGray09
 import com.lyrics.feelin.presentation.designsystem.theme.LocalFeelinColors
 import com.lyrics.feelin.presentation.view.component.music.MusicComponent
-import com.lyrics.feelin.presentation.view.component.music.MusicComponentData
+import com.lyrics.feelin.presentation.view.component.music.MusicComponentState
 import com.lyrics.feelin.presentation.view.component.profile.ProfileComponent
 import com.lyrics.feelin.util.compareNowToUser
 
@@ -95,7 +93,7 @@ fun NoteComponent(noteData: NoteComponentData, modifier: Modifier = Modifier) {
             }
         }
         MusicComponent(
-            data = MusicComponentData(
+            state = MusicComponentState.NoteComponent(
                 imageUrl = noteData.song.imageUrl,
                 songName = noteData.song.name,
                 artistName = noteData.song.artist.name,

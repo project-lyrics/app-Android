92aa115710a59f3b915229cdec779debdfd13a0f
gdaegeun539 <gdaegeun539@gmail.com>
2026-01-10 01:07:29 +0900

    refactor: update code lint

--- app/src/main/java/com/lyrics/feelin/core/designsystem/icon/Icons.kt
+++ app/src/main/java/com/lyrics/feelin/core/designsystem/icon/Icons.kt
@@ -1,7 +1,6 @@
 package com.lyrics.feelin.core.designsystem.icon
 
 import androidx.compose.runtime.Composable
-
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import com.lyrics.feelin.R

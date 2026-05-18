c7fd12aef33ffd5baa5d503459d4598b8e844fe1
gdaegeun539 <gdaegeun539@gmail.com>
2026-05-18 21:00:04 +0900

    fix: Consume logout overlay input accessibly
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- app/src/main/java/com/lyrics/feelin/presentation/view/mypage/setting/SettingScreen.kt
+++ app/src/main/java/com/lyrics/feelin/presentation/view/mypage/setting/SettingScreen.kt
@@ -2,8 +2,6 @@ package com.lyrics.feelin.presentation.view.mypage.setting
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -22,12 +20,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lyrics.feelin.R
@@ -140,18 +140,26 @@ fun SettingScreen(
 }
 
 @Composable
-private fun LogoutLoadingOverlay() {
+private fun LogoutLoadingOverlay(
+    modifier: Modifier = Modifier,
+) {
     val feelinColors = LocalFeelinColors.current
 
     Box(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .background(feelinColors.dim)
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = null,
-                onClick = {},
-            ),
+            .clearAndSetSemantics { }
+            .pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        val event = awaitPointerEvent(PointerEventPass.Initial)
+                        event.changes.forEach { pointerInputChange ->
+                            pointerInputChange.consume()
+                        }
+                    }
+                }
+            },
         contentAlignment = Alignment.Center,
     ) {
         CircularProgressIndicator()

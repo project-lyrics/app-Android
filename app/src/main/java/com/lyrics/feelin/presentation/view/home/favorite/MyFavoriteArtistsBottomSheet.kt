cad12d4c4168998cc7320c9379a587b15f33259a
gdaegeun539 <gdaegeun539@gmail.com>
2026-07-05 15:35:23 +0900

    design: Fix title alignment in MyFavoriteArtistsBottomSheet

--- app/src/main/java/com/lyrics/feelin/presentation/view/home/favorite/MyFavoriteArtistsBottomSheet.kt
+++ app/src/main/java/com/lyrics/feelin/presentation/view/home/favorite/MyFavoriteArtistsBottomSheet.kt
@@ -2,10 +2,12 @@ package com.lyrics.feelin.presentation.view.home.favorite
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -17,6 +19,7 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -51,14 +54,16 @@ fun MyFavoriteArtistsBottomSheet(
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp),
         ) {
-            Box(
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 24.dp)
+                    .padding(vertical = 28.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
                 IconButton(
                     onClick = onDismissRequest,
-                    modifier = Modifier.align(Alignment.CenterStart)
+                    modifier = Modifier.size(24.dp)
                 ) {
                     Icon(
                         imageVector = CloseIcon,
@@ -66,12 +71,16 @@ fun MyFavoriteArtistsBottomSheet(
                         tint = feelinColors.gray09
                     )
                 }
+
                 Text(
                     text = "나의 관심 아티스트",
                     style = FeelinTypography.heading3,
                     color = feelinColors.gray08,
-                    modifier = Modifier.align(Alignment.Center)
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.weight(1f)
                 )
+
+                Spacer(modifier = Modifier.size(24.dp))
             }
 
             LazyVerticalGrid(
